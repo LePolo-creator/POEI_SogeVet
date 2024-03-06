03eb189e64ddcf1cfe46543dfdd4532b7e14f9f9
Paul GENETTE <paul.genette@hotmail.fr>
2024-03-06 10:26:11 +0100

    Ajout de seeds products, user et category Modification du type de unitprice d'un product, deciaml deviens float

--- SogeVet.Server/Data/StoreContext.cs
+++ SogeVet.Server/Data/StoreContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SogeVet.Server.Entities;
+using System.Net;
 
 namespace SogeVet.Server.Data
 {
@@ -17,7 +18,49 @@ namespace SogeVet.Server.Data
         {
             optionsBuilder.UseSqlServer("Data Source =(localdb)\\MSSQLLocalDB; Initial Catalog = SogeVetDB");
         }
-       
-    
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var users = new User[]
+            {
+                new User{ Id = 1, FirstName= "Admin1", LastName= "Admin1L", Email= "MailAdmin1" , Password= "PasswordAdmin1", Address= "addressadmin1", IsAdmin= true },
+                new User{ Id = 2, FirstName= "Admin2", LastName= "Admin2L", Email= "MailAdmin2" , Password= "PasswordAdmin2", Address= "addressadmin2", IsAdmin= true },
+                new User{ Id = 3, FirstName= "User1", LastName= "User1L", Email= "MailUser1" , Password= "PasswordUser1", Address= "addressUser1", IsAdmin= false },
+                new User{ Id = 4, FirstName= "User2", LastName= "User2L", Email= "MailUser2" , Password= "PasswordUser2", Address= "addressUser2", IsAdmin= false },
+                new User{ Id = 5, FirstName= "User3", LastName= "User3L", Email= "MailUser3" , Password= "PasswordUser3", Address= "addressUser3", IsAdmin= false },
+
+            };
+            modelBuilder.Entity<User>().HasData(users);
+            var categories = new Category[]
+            {
+                new Category{ Id = 1,Name = "Homme"},
+                new Category{ Id = 2,Name = "Femme"},
+                new Category{ Id = 3,Name = "Enfant"},
+
+            };
+            modelBuilder.Entity<Category>().HasData(categories);
+            var products = new Product[]
+            {
+                new Product{ Id = 1,Name = "Produit 1" ,Description = "Description du produit 1" , Size = 30 ,UnitPrice = 20.5f , Color = "Blanc", Images = new List<string>{"URL1.1","URL1.2","URL1.3"},Quantity = 10, CategoryId = 1},
+                new Product{ Id = 2,Name = "Produit 2" ,Description = "Description du produit 2" , Size = 41 ,UnitPrice = 19.58f , Color = "Noir", Images = new List<string>{"URL2.1","URL2.2","URL2.3"},Quantity = 12, CategoryId = 2},
+                new Product{ Id = 3,Name = "Produit 3" ,Description = "Description du produit 3" , Size = 52 ,UnitPrice = 10.2f , Color = "Rouge", Images = new List<string>{"URL3.1","URL3.2","URL3.3"},Quantity = 40, CategoryId = 3},
+                new Product{ Id = 4,Name = "Produit 4" ,Description = "Description du produit 4" , Size = 32 ,UnitPrice = 40.5f , Color = "Rouge", Images = new List<string>{"URL4.1","URL4.2","URL4.3"},Quantity = 60, CategoryId = 3},
+                new Product{ Id = 5,Name = "Produit 5" ,Description = "Description du produit 5" , Size = 36 ,UnitPrice = 1.58f , Color = "Vert", Images = new List<string>{"URL5.1","URL5.2","URL5.3"},Quantity = 100, CategoryId = 2},
+                new Product{ Id = 6,Name = "Produit 6" ,Description = "Description du produit 6" , Size = 38 ,UnitPrice = 3.22f , Color = "Bleu", Images = new List<string>{"URL6.1","URL6.2","URL6.3"},Quantity = 2, CategoryId = 1},
+                new Product{ Id = 7,Name = "Produit 7" ,Description = "Description du produit 7" , Size = 20 ,UnitPrice = 3.5f , Color = "Violet", Images = new List<string>{"URL7.1","URL7.2","URL7.3"},Quantity = 12, CategoryId = 2},
+                new Product{ Id = 8,Name = "Produit 8" ,Description = "Description du produit 8" , Size = 27 ,UnitPrice = 10.58f , Color = "Vert", Images = new List<string>{"URL8.1","URL8.2","URL8.3"},Quantity = 13, CategoryId = 3},
+                new Product{ Id = 9,Name = "Produit 9" ,Description = "Description du produit 9" , Size = 25 ,UnitPrice = 6.2f , Color = "Marron", Images = new List<string>{"URL9.1","URL9.2","URL9.3"},Quantity = 24, CategoryId = 1},
+                new Product{ Id = 10,Name = "Produit 10" ,Description = "Description du produit 10" , Size = 8 ,UnitPrice = 79.99f , Color = "Turquoise", Images = new List<string>{"URL10.1","URL10.2","URL10.3"},Quantity = 52, CategoryId = 3},
+                new Product{ Id = 11,Name = "Produit 11" ,Description = "Description du produit 11" , Size = 10 ,UnitPrice = 19.58f , Color = "Vert", Images = new List<string>{"URL11.1","URL11.2","URL11.3"},Quantity = 62, CategoryId = 1},
+                new Product{ Id = 12,Name = "Produit 12" ,Description = "Description du produit 12" , Size = 30 ,UnitPrice = 3.22f , Color = "Noir", Images = new List<string>{"URL12.1","URL12.2","URL12.3"},Quantity = 5, CategoryId = 2},
+            };
+            modelBuilder.Entity<Product>().HasData(products);
+
+
+
+
+        }
+
+
     }
 }

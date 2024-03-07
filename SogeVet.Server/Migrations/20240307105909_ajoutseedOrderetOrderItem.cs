17d1d9c69ac3a9ff28faf0b78febf73b9df0d22d
Paul GENETTE <paul.genette@hotmail.fr>
2024-03-07 12:02:13 +0100

    Ajout de seeds Order et orderItems

---
+++ SogeVet.Server/Migrations/20240307105909_ajoutseedOrderetOrderItem.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace SogeVet.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class ajoutseedOrderetOrderItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "OrderItems",
+                columns: new[] { "Id", "OrderId", "ProductId", "Quantity", "UnitPrice" },
+                values: new object[,]
+                {
+                    { 1, 1, 7, 2, 20 },
+                    { 4, 1, 2, 10, 5 },
+                    { 6, 1, 10, 2, 5 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Orders",
+                columns: new[] { "Id", "Address", "Status", "UserId" },
+                values: new object[,]
+                {
+                    { 2, "Adress 2", "En attente de traitement", 4 },
+                    { 3, "Adress 3", "En attente de traitement", 5 },
+                    { 4, "Adress 4", "En attente de traitement", 3 },
+                    { 5, "Adress 5", "En attente de traitement", 3 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "OrderItems",
+                columns: new[] { "Id", "OrderId", "ProductId", "Quantity", "UnitPrice" },
+                values: new object[,]
+                {
+                    { 2, 2, 4, 3, 25 },
+                    { 3, 3, 3, 4, 26 },
+                    { 5, 2, 1, 1, 80 },
+                    { 7, 4, 11, 3, 40 },
+                    { 8, 5, 12, 2, 15 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "OrderItems",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "OrderItems",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "OrderItems",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "OrderItems",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "OrderItems",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "OrderItems",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "OrderItems",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "OrderItems",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}

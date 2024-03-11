827fc087af08d52e61ee31c18c22421a7828c32f
cameliaasb <camelia.assobhei@gmail.com>
2024-03-11 10:51:03 +0100

    User : rajout col isActive + récupérer les orders par user

--- SogeVet.Server/Entities/Order.cs
+++ SogeVet.Server/Entities/Order.cs
@@ -16,7 +16,7 @@ namespace SogeVet.Server.Entities
         public OrderDto ConvertToDto()
         {
            var orderItemsDto = new List<OrderItemDto>();
-           foreach (var item in this.OrderItems) { orderItemsDto.Add(item.ConvertToDto()); };
+           foreach (var item in OrderItems) { orderItemsDto.Add(item.ConvertToDto()); };
            return new OrderDto() { Id = this.Id, Address = this.Address, Status = this.Status, OrderItems = orderItemsDto, UserId = this.UserId };
         }
     }
--- SogeVet.Server/Entities/User.cs
+++ SogeVet.Server/Entities/User.cs
@@ -12,14 +12,20 @@ namespace SogeVet.Server.Entities
         public string Address { get; set; } = string.Empty;
 
         public bool IsAdmin { get; set; } = false;
+        public bool IsActive { get; set; } = true;
 
 
-        public List<Order>? Orders { get; set; }
+        public List<Order> Orders { get; set; } = new List<Order>();
 
 
         public UserDto ConvertToDto()
         {
-            return new UserDto() { Id = this.Id, FirstName = this.FirstName, LastName = this.LastName, Email = this.Email, Password = this.Password,Address = this.Address,IsAdmin = this.IsAdmin };
+            var ordersDto = new List<OrderDto>();
+            foreach (var order in Orders) { ordersDto.Add(order.ConvertToDto()); }
+            return new UserDto() { Id = this.Id, FirstName = this.FirstName, LastName = this.LastName, 
+                Email = this.Email, Password = this.Password,Address = this.Address,
+                IsAdmin = this.IsAdmin, IsActive = this.IsActive, Orders = ordersDto
+               };
         }
 
    

73f8c2f1b42f69403f45f1ddada240202459491e
Paul GENETTE <paul.genette@hotmail.fr>
2024-03-11 16:33:09 +0100

    Modification et ajout de la creation de token pour l'authetification

--- SogeVet.Server/Program.cs
+++ SogeVet.Server/Program.cs
@@ -1,4 +1,7 @@
 using SogeVet.Server.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddCors(
@@ -12,11 +15,31 @@ builder.Services.AddCors(
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson();
 builder.Services.AddDbContext<StoreContext>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+//Configurer le service d'auhtentification
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Authentication:Issuer"],
+            ValidAudience = builder.Configuration["Authentication:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(
+               Convert.FromBase64String(builder.Configuration["Authentication:SecretForKey"]))
+        };
+    }
+);
+
+
 
 var app = builder.Build();
 
@@ -32,6 +55,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseCors("AdminClient");
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();

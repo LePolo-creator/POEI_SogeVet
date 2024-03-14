be8772b48e2ee5ac656d45dd9d4fe99af7bcabf0
Paul GENETTE <paul.genette@hotmail.fr>
2024-03-14 14:50:57 +0100

    Ajout gestion auth + ID du user

--- sogevet.client/src/app/login/service/login.service.ts
+++ sogevet.client/src/app/login/service/login.service.ts
@@ -7,14 +7,7 @@ import { Router } from '@angular/router';
 })
 export class LoginService {
   baseUrl = "https://localhost:7265/api/login";
-  options = {
-    headers: new HttpHeaders(
-      {
-        "content-type": "application/json",
-        "authorization": "Bearer" + localStorage.getItem("token") || ""
-      }
-    )
-  }
+  
 
   constructor(private http: HttpClient, private router: Router
   ) { }
@@ -25,16 +18,25 @@ export class LoginService {
       Password: password
     })
 
+    const options = {
+      headers: new HttpHeaders(
+        {
+          "content-type": "application/json"
+        }
+      )
+    }
+
+
     console.log(body);
-    this.http.post(this.baseUrl, body, this.options).subscribe(
+    this.http.post(this.baseUrl, body, options).subscribe(
       {
         next: (response: any) => {
           //console.log(response);
           //Récupérer le token renvoyé par l'API serveur
-          const authToken = (<any>response).token;
+          const authToken = (<any>response);
 
           //Enregistrer le token dans localstorage
-          localStorage.setItem("token", authToken);
+          localStorage.setItem("authSogevet", JSON.stringify(authToken));
 
           //une fois que l'utilisateur est connecté, je le redirige vers la liste des produits
           this.router.navigate(["/"]);
@@ -47,11 +49,15 @@ export class LoginService {
 
 
   isAuthenticated(): boolean {
-    var token = localStorage.getItem("token")
-    if (token == undefined
+    if (localStorage.getItem("authSogevet") == null) {
+      console.log("dans null")
+      return false;
+    }
+    var tokenAuth = JSON.parse(localStorage.getItem("authSogevet")!).token
+    console.log(tokenAuth);
+    if (tokenAuth == undefined
     )
       return false;
-    //Il recommandé d'ajouter une requête vers le serveur afin de vérifier la validité du token
     return true;
   }
 }
--- sogevet.client/src/app/user/user-routing.module.ts
+++ sogevet.client/src/app/user/user-routing.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserAccountComponent } from './user-account/user-account.component';
 import { UserOrdersComponent } from './user-orders/user-orders.component';
+import { authGuard } from '../shared/auth.guard';
 
 
 
 const routes: Routes = [
   {
-    path: "", component: UserAccountComponent
+    path: "", component: UserAccountComponent, canActivate: [authGuard]
   },
   {
-    path: "orders", component: UserOrdersComponent
+    path: "orders", component: UserOrdersComponent, canActivate: [authGuard]
   }
 
 ];
